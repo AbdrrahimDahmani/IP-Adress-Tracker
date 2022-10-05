89c1b3b414126ace620204cc86afe6adf8cf2086
AbdrrahimDahmani <abdrrahim.dahmani.2002@gmail.com>
2022-10-06 00:59:46 +0100

    code improved

--- src/app/services/ip-tracker.service.ts
+++ src/app/services/ip-tracker.service.ts
@@ -8,8 +8,8 @@ import { geoLocation } from '../models/geoLocation.model';
   providedIn: 'root'
 })
 export class IpTrackerService {
-  apiKey=environment.apiKey
-  baseApiUrl=environment.baseApiUrl
+  apiKey="at_IgRBI40aArF3PnW2TiTUvxh1JThG5"
+  baseApiUrl="https://geo.ipify.org/api/v2/country,city?apiKey="
   constructor(private http:HttpClient) { }
   getIpTrackInfos(ip:any):Observable<geoLocation>{
     return this.http.get<geoLocation>(this.baseApiUrl+this.apiKey+"&ipAddress="+ip)
