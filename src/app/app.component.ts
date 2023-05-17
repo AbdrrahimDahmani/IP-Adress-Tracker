e5c995dfb5aed7becbd0e191d098856eaa4fcfb1
abdrrahimdahmani <abdrrahim.dahmani.2002@gmail.com>
2023-05-17 12:33:33 +0100

    Updated prod environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IP_Adress_Tracker';
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  ipAdress:'',
+  baseApiUrl:'',
+  apiKey:'at_h9fk3Kkhy6KSignRLMJj1x1wzoIPs',
+  isChecked: false
 };
